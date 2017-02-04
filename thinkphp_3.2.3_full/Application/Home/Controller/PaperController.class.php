98506bd54a2e598c5edccbce4b217f0f8c3f5265
wangwei <569469771@qq.com>
2017-02-04 16:08:09 +0800

    纸板

--- thinkphp_3.2.3_full/Application/Home/Controller/PaperController.class.php
+++ thinkphp_3.2.3_full/Application/Home/Controller/PaperController.class.php
@@ -20,19 +20,77 @@ class PaperController extends FatherController {
 		$this->assign('list',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->assign('suplist',$suplist);
-		
 		$this->display('Paper');
-		
 	}
 	public function addPaper(){
-
-		$this->display('Addpaper');
-		
+		if($_POST && cookie('name')){
+			// dump($_POST);die;
+			$data=[];
+			$data['sup_id']=I('post.sup_id');
+			$data['paper_property']=I('post.paper_property');
+			$data['paper_name']=I('post.paper_name');
+			$data['paper_price']=floatval(I('post.paper_price'));
+			$data['paper_state']=I('post.paper_state');
+			$data['up_date'] = time();
+			// dump($data);die;
+			$pap= D('Paper');
+			$pflag=$pap->getPaper($data['sup_id'],$data['paper_property']);
+			if($pflag){
+				$this->error('已有相同数据！','/Home/Paper/index/',3);
+			}else{
+				$inflag=$pap->addPaper($data);
+				if($inflag){
+					$this->success('添加成功', '/Home/Paper/index/',1);	
+				}else{
+					$this->error('添加失败！','/Home/Paper/index/',3);
+				}
+			}
+		}else{
+			$sup = D('Supplier');
+			$suplist = $sup->getAll();
+			$this->assign('suplist',$suplist);
+			$this->display('Addpaper');
+		}
 	}
-	public function editPaper(){
-
-		$this->display('Editpaper');
-		
+	public function editPaper($id='0'){
+		$id=intval(I('get.id'));
+		if($id=='0'){
+			$this->error('ID不存在！','/Home/Paper/index/',3);
+		}else{
+			if(empty($_POST)){
+				$pap= D('Paper');
+				$arr = $pap->getById($id);
+				if($arr){
+					$sup = D('Supplier');
+					$suplist = $sup->getAll();
+					$this->assign('suplist',$suplist);
+					$this->assign('arr',$arr);
+					$this->display('Editpaper');
+				}else{
+					$this->error('ID不存在！','/Home/Paper/index/',3);
+				}
+			}else{
+				$data=[];
+				$data['sup_id']=I('post.sup_id');
+				$data['paper_property']=I('post.paper_property');
+				$data['paper_name']=I('post.paper_name');
+				$data['paper_price']=floatval(I('post.paper_price'));
+				$data['paper_state']=I('post.paper_state');
+				$data['up_date'] = time();
+				$pap= D('Paper');
+				$pflag=$pap->getById($id);
+				if($pflag){
+					$inflag=$pap->editData($id,$data);
+					if($inflag){
+						$this->success('修改成功', '/Home/Paper/index/',1);	
+					}else{
+						$this->error('修改失败！','/Home/Paper/index/',3);
+					}
+				}else{
+					$this->error('ID不存在！','/Home/Paper/index/',3);
+				}
+			}
+		}
 	}
 	
 }
--- thinkphp_3.2.3_full/Application/Home/Model/PaperModel.class.php
+++ thinkphp_3.2.3_full/Application/Home/Model/PaperModel.class.php
@@ -5,12 +5,28 @@ class PaperModel extends Model {
 	// protected $tableName = 'categories';
 	
 	//实例化模型
-	public function getUser(){
-		$User = M("User"); // 实例化User对象
-		$User->table('my_user')->where('name="ww1985"')->find();
+	public function getPaper($sup_id,$paper_property){
+		$pa = M("Paper"); // 实例化User对象
+		$flag=$pa->
+		where("`sup_id` = '".$sup_id."' and `paper_property` = '".$paper_property."'")
+		->find();
 		// $result=$User->result();
-	
-		return $User->data;
 		// echo $this->getLastSql();die;
+		return $flag;
+	}
+	public function getById($id){
+		$pa = M("Paper");
+		$data=$pa->where("`id` = '".$id."'")->find();
+		return $data;
+	}
+	public function addPaper($data){
+		$pa = M("Paper");
+		$flag=$pa->data($data)->add();
+		return $flag;
+	}
+	public function editData($id,$data){
+		$pa = M("Paper");
+		$flag=$pa->where("`id` = '".$id."'")->data($data)->save();
+		return $flag;
 	}
 }
