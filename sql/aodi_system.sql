06fa6a533226e43c02736786daf422bf024e3a9e
wangwei <569469771@qq.com>
2017-04-29 09:12:23 +0800

    112

--- sql/aodi_system.sql
+++ sql/aodi_system.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `aodi_system`
+-- Database: `aodi_system` 
 --
 
 -- --------------------------------------------------------
