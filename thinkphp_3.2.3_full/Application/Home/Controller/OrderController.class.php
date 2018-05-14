aa7c6870ca576d708c758fc1826d1d6b0a7e677e
wangwei <569469771@qq.com>
2018-05-14 14:55:12 +0800

    订单controller

--- thinkphp_3.2.3_full/Application/Home/Controller/OrderController.class.php
+++ thinkphp_3.2.3_full/Application/Home/Controller/OrderController.class.php
@@ -47,35 +47,47 @@ class OrderController extends FatherController {
 		if($_POST){
 			dump($_POST);die;
 			//表1
+			//订单是否是取消的，默认不是取消的
+			// $order_state =
+			//工单号
+			// $job_num =
 			// $customer_code = I("post.customer_code");
 			// $customer_id = I("post.customer_s");
 			// $customer_name = I("post.cus_name");
 			// $company_id = cookie('cd');
 			// $mant_num = I("post.mant_num");
-			// $job_num =
 			// $order_urgent = I("post.order_urgent");
-			// $order_state =
 			// $order_num = I("post.order_num");
-			// $order_price =
-			// $order_otherprice =
-			// $is_paper =
-			// $up_uid =
-			// $del_date = 
+			 //一平方纸板价格
+			// $order_price =I("post.order_price");
+			// $order_otherprice =I("post.order_otherprice");
+			// $is_paper =I("post.is_paper");
+			//修改者id
+			// $up_uid = cookie("ud");
+			// $del_date = I("post.del.date");
 			
 			//表2
 			// $orderone_id =
-			// $shg_name =
-			// $today_price =
-			// $box_long =
-			// $box_height =
-			// $box_width =
-			// $paper_property
-			// $box_prop =
-			// $box_volume =
-			// $map_color =
-			// $in_uid =
-			// $up_date =
-			// $order_map =
+			//商品名
+			// $shg_name = I("post.shg_name");
+			//订单总价
+			// $all_price =;
+			// $box_long =I("post.box_long");
+			// $box_height =I("post.box_height");
+			// $box_width =I("post.box_width");
+			// $paper_property = I("post.paper_property");
+			//纸箱材质
+			// $box_prop = I("post.box_prop");
+			//纸箱单个体积
+			// $box_volume = ;
+			//几色印刷
+			// $map_color = I("post.map_color");
+			//添加者id
+			// $in_uid = cookie("ud");
+			//输单时间 (当前时间)
+			// $up_date = 
+			//是否要做图
+			// $order_map = I("post.order_map");
 	
 		}else{
 			$o_cus = D('Customer');
